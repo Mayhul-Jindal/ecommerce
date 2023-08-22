9fe38ef62ea9bd6e7a510abd67129dbf7c875df8
Mayhul-Jindal <mayhuljindal@gmail.com>
2023-08-22 19:07:28 +0530

    feat: seperated logging service from bussiness logic

---
+++ logger.go
@@ -0,0 +1,69 @@
+// TODO
+/*
+- integrate zap/zerolog library into this for log rotation 
+- Database ke liye shayad alag se banega
+*/
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/BalkanID-University/vit-2025-summer-engineering-internship-task-Mayhul-Jindal.git/types"
+)
+
+/*
+This basically demonstrates how the logging service can be seperated from the bussiness
+logic which can therefore increase maintainability.
+*/
+type loggingService struct {
+	next BookManager
+}
+
+func NewLoggingService(svc BookManager) BookManager {
+	return &loggingService{
+		next: svc,
+	}
+}
+
+func (l *loggingService) Search(ctx context.Context, query string) (books []types.Book, err error) {
+	defer func(begin time.Time) {
+		log.Printf("books: %v\terr: %v", books, err)
+	}(time.Now())
+
+	return l.next.Search(ctx, query)
+}
+
+func (l *loggingService) Filter(ctx context.Context, contraints string) (books []types.Book, err error) {
+	defer func(begin time.Time) {
+		log.Printf("books: %v\terr: %v\n", books, err)
+	}(time.Now())
+
+	return l.next.Filter(ctx, contraints)
+}
+
+func (l *loggingService) AddtoCart(ctx context.Context, book types.Book) (err error) {
+	defer func(begin time.Time) {
+		log.Printf("err: %v\n", err)
+	}(time.Now())
+
+	return l.next.AddtoCart(ctx, book)
+}
+
+func (l *loggingService) DownloadBooks(ctx context.Context, books []types.Book) (err error) {
+	defer func(begin time.Time) {
+		log.Printf("err: %v\n", err)
+	}(time.Now())
+
+	return l.next.DownloadBooks(ctx, books)
+}
+
+func (l *loggingService) ReviewBook(ctx context.Context, book types.Book, review types.Review) (err error) {
+	defer func(begin time.Time) {
+		log.Printf("err: %v\n", err)
+	}(time.Now())
+
+	return l.next.ReviewBook(ctx, book, review)
+}
